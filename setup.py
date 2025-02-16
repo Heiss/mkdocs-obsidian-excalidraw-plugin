da79fa1e73e17ad571e07987bd9fceb447bef2ae
Peter Heiss <peter.heiss@uni-muenster.de>
2025-02-16 20:51:49 +0100

    rename publish action name

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='mkdocs-obsidian-excalidraw-plugin',
     version='0.1.0',
     description='A MkDocs plugin',
-    long_description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords='mkdocs,excalidraw,obsidian',
     url='',
     author='Peter Heiss',
